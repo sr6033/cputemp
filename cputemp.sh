0e0bf473ea9656e79f3ffabc5c670eacb4fe7157
Shubham Rath <shubhamrath.iiitb@gmail.com>
2016-05-14 20:12:01 +0530

    Create cputemp.sh

---
+++ cputemp.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+while :
+do
+	cputemp0=`cat /sys/class/thermal/thermal_zone0/temp`
+	cputemp1=$(($cputemp0/1000))
+	cputemp2=$(($cputemp0/100))
+	cputemp=$(($cputemp2%$cputemp1))
+	if [ $cputemp1>50 ]
+		then
+			notify-send "COOL DOWN!($cputemp1 'C)"
+	fi
+	sleep 15m
+done
